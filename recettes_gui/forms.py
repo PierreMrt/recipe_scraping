33189b2461d5f3bbf75734aaa05e954305784694
PierreMrt <merlet.pierre@outlook.com>
2021-11-28 23:02:04 +0100

    started working on django gui and search form

--- recettes_gui/forms.py
+++ recettes_gui/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search = forms.CharField(label='Recettes', max_length=100)
+    search = forms.CharField(label='recherche', max_length=100)
--- recettes_gui/views.py
+++ recettes_gui/views.py
@@ -1,44 +1,26 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from .forms import SearchForm
+from django.http import HttpResponseRedirect
+
+from .models import Recipe
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
 
-class SearchCreateView(CreateView):
-    model = Search
-    fields = ['job', 'country']
-    template_name = 'jobs/search_form.html'
-    success_url = '/'
-    
-    def form_valid(self, form):
-        # process the data in form.cleaned_data
-        job = form.cleaned_data['job']
-        country = form.cleaned_data['country']
-        # here we need to pass the data to the model's methods
-        job = job.replace(" ", "_").lower()
-        country = country.replace(" ", "_").lower()
+def get_search(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
 
-        active_search = Search.objects.filter(Q(job=job), Q(country=country))
-        if len(active_search) > 0:
-            print('add user')
-            active_search[0].user.add(self.request.user)
-        else:
-            print('create search')
-            s = Search(job=job, country=country, update_date=datetime.now(tz=timezone.utc))
-            s.save()
-            s.user.add(self.request.user)
-        update_search(None, job=job, country=country)
-        return redirect('/')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
 
-    def get(self, request):
-        if not request.user.is_authenticated:
-            return redirect('/login')
-        else:
-            return super().get(request)
-
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        if self.request.accepts('text/html'):
-            return response
-        else:
-            return JsonResponse(form.errors, status=400)
+    return render(request, 'index.html', {'form': form})
